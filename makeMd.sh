36f018d770d90c4d1e5168218768fc78bec509f8
SomuSysAdmin <somu.sysadm@gmail.com>
2018-01-04 20:22:05 +0530

    Added files to help automate the generation of GFM from Latex files

--- makeMd.sh
+++ makeMd.sh
@@ -35,11 +35,25 @@ echoCol() {
 # Setting the base directory
 if [ "$#" -lt 1 ]; then
 	baseDir="$(pwd)"
-	copyDir="/home/somu/VM/markdown_notes"
+	copyDir="/vm/markdown_notes"
+elif [ "$#" -lt 2 ]; then
+	baseDir="$1"
+	copyDir="/vm/markdown_notes"
 else
 	baseDir="$1"
 	copyDir="$2"
 fi
+
+if [ "$3" = "--remove" ]; then
+	echoCol "3" "WARNING: " "Deleted contents of $copyDir"
+	rm -rf $copyDir/*
+fi
+
+echo "Basedir: $baseDir"
+echo "Copydir: $copyDir"
+subGroup=$(echo "$baseDir" | sed -E "s|.*\/(.*)|\1|g")
+mkdir "$copyDir/$subGroup" || echoErr "Error creating directory $subGroup @ $copyDir"
+echoCol 4 "Info: " "Created Directory ${BLUE}$copyDir/$subGroup${NC}"
 
 # Copying all directories, and translating all files from LaTeX to Markdown using Pandoc
 function traverse() {
@@ -48,9 +62,16 @@ function traverse() {
 	    [ "$f" = "$1" ] && continue
 		if [ -d "$f" ]; then
         	# echo "** Directory $f **"
-			relpath=$(sed -E "s/$baseDir\///g" "$f")
+			relpath=$(echo "$f" | sed -E "s|$baseDir||g")
+			relpath="$subGroup$relpath"
+			#echo "f = $f"
+			#echo "Relpath = $relpath"
 			location="$copyDir/$relpath"
-			mkdir "$location"
+			#echo "Copydir+Relpath = $location"
+			location=$(echo "$location" | sed -E "s|(.*)/chapters(.*)|\1\2|g")
+			#echo "Location = $location"
+			mkdir "$location" || echo -e "${RED}ERROR:${NC} Can't create directory $location"
+			echoCol 4 "Info: " "Created Directory ${BLUE}$location${NC}"
         	traverse "$f"
     	else
 			# Making the Chapter Heading from file name :
@@ -59,14 +80,16 @@ function traverse() {
 			if [ -n "$name" ]; then # if a valid texfile within the `chapters` Directory is found
         		# echo "$f"
 				# echoCol 3 "├─Prefix : " "$pre"
+				name=$(echo "$name" | tr '/' '-')
 				name=$(echo "Chapter $pre. $name")
 				echoCol 2 "$name" ""
 				#echo "$name"
-				mkdir "$name" || echoErr "Can't create directory $name @" $(pwd)
+				mkdir "$location/$name" || echoErr "Can't create directory $name @ $location"
+				echoCol 4 "Info: " "Created Directory ${BLUE}$location/$name${NC}"
 
 				# Analyzing File `$f` to find sections and name them :
 				count=0
-				echo "" > temp.tex 	# Clearing a temporary tex file to contain the passage.
+				echo -n "" > temp.tex 	# Clearing a temporary tex file to contain the passage.
 				while read -r line
 				do
 					# Checking if the current line is a section.
@@ -74,15 +97,16 @@ function traverse() {
 			    	head=$(echo -n "$head" | sed -E "s/\\\_/_/g")
 			    	head=$(echo -n "$head" | sed -E "s/\n//g")
 					if [ -n "$head" ]; then
-						[ -s temp.tex ] && mkGfm "$copydir/$name/$fullname"
+						[ -s temp.tex ] && mkGfm "$location/$name/$fullName"
 						count=$(( $count+1 ))
-					    echoCol 4 "  $count. $head" ""
 						fullName="$count. $head"
-					elif [ count -gt 0 ]; then
+						fullName=$(echo "$fullName" | tr '/' '-')
+					    echoCol 4 "  $fullName" ""
+					elif [ $count -gt 0 ]; then
 						echo "$line" >> temp.tex
 					fi
 				done < "$f"
-				[ -s temp.tex ] && mkGfm "$copyDir/$name/$fullname"
+				[ -s temp.tex ] && mkGfm "$location/$name/$fullName"
 			fi
 	   	fi
 	done
@@ -99,8 +123,15 @@ function numExt() {
 
 function mkGfm () {
 	# Turns the temporary tex file `temp.tex` to a GFM Marddown file whose name is ($1).md
-	pandoc -f latex -t gfm -o "$1.md" temp.tex
-	echo "" > temp.tex
+	err=0
+	pandoc -f latex -t gfm -o "$1.md" temp.tex || err=1
+	if [ "$err" -eq 1 ]; then
+		echo -e "${RED}ERROR:${NC} Can't create file, EXITING!!!"
+		echo -e "\nDUMP: \n$1\n\nTEMP: "
+		cat temp.tex
+		exit
+	fi
+	echo -n "" > temp.tex
 }
 
 traverse $baseDir
